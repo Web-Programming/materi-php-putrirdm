f9649c9744ef83a65972e80920d5d16ae7e88666
putrirdm <putriredamaulidia_2428240086@mhs.mdp.ac.id>
2026-05-20 15:02:41 +0700

    upp

--- putriredamaulidia-app/database/migrations/2026_05_05_045808_create_products_table.php
+++ putriredamaulidia-app/database/migrations/2026_05_05_045808_create_products_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->decimal('price', 10, 2);
+            $table->text('description')->nullable();
+            $table->enum('status',['new', 'used'])->default('new');
+            $table->boolean('is_active')->default(true);
+            $table->date('release_date')->nullable();
+            //menambahkan kolom created_at dan updated_at secara otomatis
             $table->timestamps();
         });
     }
--- putriredamaulidia-app/database/migrations/2026_05_13_095408_create_supplier_table.php
+++ putriredamaulidia-app/database/migrations/2026_05_13_095408_create_supplier_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 150);
+            $table->string('phone', 15);
+            $table->text('address')->nullable();
             $table->timestamps();
         });
     }
